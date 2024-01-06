7a13d1bd423c22418ce28b473783fb4116cfcc5a
kalpit10 <kalpit.swami@gmail.com>
2024-01-06 16:32:25 +0530

    Changed few fonts and background image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,9 @@ import { Link } from "react-router-dom";
 function Header(props) {
   return (
     <div
-      className={`header dark-heading ${props.isNavbarSticky ? "navbar-sticky" : ""}
+      className={`header dark-heading ${
+        props.isNavbarSticky ? "navbar-sticky" : ""
+      }
       }`}
     >
       <ul className="navbar navbar-light justify-content-center">
@@ -23,34 +25,7 @@ function Header(props) {
             My Resume
           </Link>
         </li>
-
-        {/* <li className="nav-item nav-link">
-          <label className="m-3 mx-5">Dark Mode</label>
-          <Switch
-            onClick={props.toggleDarkMode}
-            defaultChecked={props.darkMode}
-          />
-        </li> */}
       </ul>
-
-      {/* <div>
-        <div className="full-height">
-          <div className="absolute-center">
-            <div className="section">
-              <div className="container">
-                <div className="row">
-                  <div className="col-12 py-5">
-                    <h1 className="dark-span-h1">
-                      <span>{text}</span>
-                      {showCursor && <span className="cursor">|</span>}
-                    </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
--- src/components/TypeWriter.jsx
+++ src/components/TypeWriter.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import TypeWriter from "typewriter-effect";
+import "../Stylesheet/Typewriter.css";
 
 function Typewriter() {
   return (
-    <TypeWriter
-      options={{
-        strings: [
-          "MERN STACK DEVELOPER",
-          "JAVASCRIPT",
-          "REACT.JS",
-          "NODE.JS",
-          "MONGODB",
-        ],
-        delay: 70,
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+    <div className="typewriter-effect">
+      <TypeWriter
+        options={{
+          strings: [
+            "MERN STACK DEVELOPER",
+            "JAVASCRIPT",
+            "REACT.JS",
+            "NODE.JS",
+            "MONGODB",
+          ],
+          delay: 70,
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 50,
+        }}
+      />
+    </div>
   );
 }
 
