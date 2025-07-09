feae103809d30f51875d23f1123bc5bd75c53bcc
kalpit10 <kalpit.swami@gmail.com>
2025-07-09 00:15:45 -0400

    Updated Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { IoLogoJavascript } from "react-icons/io";
 import { IoLogoReact } from "react-icons/io5";
-import { FaNode, FaBootstrap, FaGit } from "react-icons/fa";
-import { FaDocker, FaLinux } from "react-icons/fa";
-import { SiExpress, SiMongodb, SiTailwindcss } from "react-icons/si";
+import { FaNode, FaGit, FaDocker, FaLinux, FaAws } from "react-icons/fa";
+import { SiExpress, SiMongodb, SiGnubash } from "react-icons/si";
 // import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../Stylesheet/Skills.css";
@@ -15,16 +14,8 @@ function Skills() {
       <h1 className="mt-4 text-center my-6 mx-auto dark-skill">TECH STACK</h1>
       <div className="row justify-content-center pb-5 py-4">
         <div className="col-xs-4 col-md-2 tech-icons">
-          <IoLogoJavascript />
-          <h5 className="mx-auto font-weight-light p-2">JavaScript</h5>
-        </div>
-        <div className="col-xs-4 col-md-2 tech-icons">
-          <IoLogoReact />
-          <h5 className="mx-auto font-weight-light p-2">React.js</h5>
-        </div>
-        <div className="col-xs-4 col-md-2 tech-icons">
-          <FaNode />
-          <h5 className="mx-auto font-weight-light p-2">Node.js</h5>
+          <FaAws />
+          <h5 className="mx-auto font-weight-light p-2">AWS</h5>
         </div>
         <div className="col-xs-4 col-md-2 tech-icons">
           <FaDocker />
@@ -35,24 +26,33 @@ function Skills() {
           <h5 className="mx-auto font-weight-light p-2">Linux</h5>
         </div>
         <div className="col-xs-4 col-md-2 tech-icons">
-          <SiExpress />
-          <h5 className="mx-auto font-weight-light p-2">Express.js</h5>
+          <FaGit />
+          <h5 className="mx-auto font-weight-light p-2">Git</h5>
+        </div>
+        <div className="col-xs-4 col-md-2 tech-icons">
+          <SiGnubash />
+          <h5 className="mx-auto font-weight-light p-2">Bash</h5>
+        </div>
+        <div className="col-xs-4 col-md-2 tech-icons">
+          <IoLogoJavascript />
+          <h5 className="mx-auto font-weight-light p-2">JavaScript</h5>
         </div>
         <div className="col-xs-4 col-md-2 tech-icons">
           <SiMongodb />
           <h5 className="mx-auto font-weight-light p-2 ">MongoDB</h5>
         </div>
         <div className="col-xs-4 col-md-2 tech-icons">
-          <FaBootstrap />
-          <h5 className="mx-auto font-weight-light p-2">Bootstrap</h5>
+          <SiExpress />
+          <h5 className="mx-auto font-weight-light p-2">Express.js</h5>
+        </div>
+
+        <div className="col-xs-4 col-md-2 tech-icons">
+          <IoLogoReact />
+          <h5 className="mx-auto font-weight-light p-2">React.js</h5>
         </div>
         <div className="col-xs-4 col-md-2 tech-icons">
-          <FaGit />
-          <h5 className="mx-auto font-weight-light p-2">Git</h5>
-        </div>
-        <div className="col-xs-4 col-md-2 tech-icons">
-          <SiTailwindcss />
-          <h5 className="mx-auto font-weight-light p-2">Tailwind</h5>
+          <FaNode />
+          <h5 className="mx-auto font-weight-light p-2">Node.js</h5>
         </div>
       </div>
     </div>
